8d17ea7eea61f44324752959ffd36721d671c627
Omkar Harade <omkarharade1301@gmail.com>
2023-10-07 23:10:18 +0530

    added controller and route file for api call

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import express from "express";
 import { connect } from "./config/database.js";
 import service from "./services/tweet-service.js";
+import bodyParser from "body-parser";
+import apiRoutes from "./routes/index.js";
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api", apiRoutes);
 
 app.listen(3000, async () => {
 	console.log("server started");
