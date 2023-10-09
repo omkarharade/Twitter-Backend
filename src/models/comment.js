e556007f63ca4b5c97598468772a896f95423f63
Omkar Harade <omkarharade1301@gmail.com>
2023-10-09 14:35:00 +0530

    added ref path to commentSchema

--- src/models/comment.js
+++ src/models/comment.js
@@ -6,8 +6,20 @@ const commentSchema = new mongoose.Schema(
 			type: String,
 			required: true,
 		},
-		userEmail: {
+		userId: {
+			ype: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		onModel: {
 			type: String,
+			required: true,
+			enum: ["Tweet", "Comment"],
+		},
+		commentable: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			refPath: "onModel",
 		},
 	},
 	{ timestamps: true }
