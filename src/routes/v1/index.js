3a81999e9110516c34c664b422334da8ae720da8
Omkar Harade <omkarharade1301@gmail.com>
2023-10-11 23:49:10 +0530

    added passport logic

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,12 +2,14 @@ import express from "express";
 
 import { createTweet, getTweet } from "../../controllers/tweet-controller.js";
 import { toggleLike } from "../../controllers/like-controller.js";
-import { signup } from "../../controllers/auth-controller.js";
+import { signup, login } from "../../controllers/auth-controller.js";
+
+import { authenticate } from "../../middlewares/authenticate.js";
 
 const router = express.Router();
 
 // tweets
-router.post("/tweets", createTweet);
+router.post("/tweets", authenticate, createTweet);
 router.get("/tweets/:id", getTweet);
 
 // likes
@@ -18,5 +20,6 @@ router.post("/comments", createComment);
 
 //auth
 router.post("/signup", signup);
+router.post("/login", login);
 
 export default router;
